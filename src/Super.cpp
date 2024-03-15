65dfe5f472b37274a6c507d0f8c6096d0819824e
Bruno Horta <bruno.horta@avanceesoftware.com>
2024-03-15 17:09:09 +0000

    super 2

--- src/Super.cpp
+++ src/Super.cpp
@@ -8,10 +8,10 @@ void super(void *args)
 {
     char *msn = (char *)args;
 
-    ESP_LOGI("SUPER", "OLAS");
+    ESP_LOGI("SUPER2", "OLAS");
     while (true)
     {
-        ESP_LOGI("SUPER", "LOOP %s", msn);
+        ESP_LOGI("SUPER2", "LOOP %s", msn);
         vTaskDelay(pdTICKS_TO_MS(10));
     }
 }
